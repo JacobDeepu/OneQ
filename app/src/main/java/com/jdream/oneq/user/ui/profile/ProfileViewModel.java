2037eb8f2aefdca66450db6829d717c3003a96f0
Jacob Deepu <d2jdeepu@gmail.com>
2021-05-22 22:18:26 +0530

    Return empty string instead of null from ViewModel
     Fixes app crash on inflating fragment

--- app/src/main/java/com/jdream/oneq/user/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/jdream/oneq/user/ui/profile/ProfileViewModel.java
@@ -1,14 +1,19 @@
 package com.jdream.oneq.user.ui.profile;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class ProfileViewModel extends ViewModel {
 
+    private MutableLiveData<String> mText;
+
     public ProfileViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("");
     }
 
     public LiveData<String> getText() {
-        return null;
+        return mText;
     }
 }
--- app/src/main/java/com/jdream/oneq/user/ui/qrcode/QrcodeViewModel.java
+++ app/src/main/java/com/jdream/oneq/user/ui/qrcode/QrcodeViewModel.java
@@ -1,14 +1,19 @@
 package com.jdream.oneq.user.ui.qrcode;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class QrcodeViewModel extends ViewModel {
 
+    private MutableLiveData<String> mText;
+
     public QrcodeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("");
     }
 
     public LiveData<String> getText() {
-        return null;
+        return mText;
     }
 }
