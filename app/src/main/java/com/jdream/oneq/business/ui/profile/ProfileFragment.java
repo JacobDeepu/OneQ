8654f540eccc6fdee7d3682c489529704e0cf5ed
Jacob Deepu <d2jdeepu@gmail.com>
2021-10-09 12:19:04 +0530

    Design UI for Business Profile

--- app/src/main/java/com/jdream/oneq/business/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/jdream/oneq/business/ui/profile/ProfileFragment.java
@@ -1,9 +1,11 @@
 package com.jdream.oneq.business.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,12 +13,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.jdream.oneq.LoginActivity;
+import com.jdream.oneq.R;
 import com.jdream.oneq.databinding.FragmentBusinessProfileBinding;
 
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
     private FragmentBusinessProfileBinding binding;
+    private Button btnLogout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +31,18 @@ public class ProfileFragment extends Fragment {
 
         binding = FragmentBusinessProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        btnLogout = root.findViewById(R.id.button_logout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent loginActivity = new Intent(getActivity(), LoginActivity.class);
+                startActivity(loginActivity);
+                requireActivity().finishAfterTransition();
+            }
+        });
 
         profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
